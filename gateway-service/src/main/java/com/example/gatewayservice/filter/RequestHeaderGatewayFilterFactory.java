348521f234bcd9725abd834044c8e789fae461ec
almak6 <maksimovi.ck@mail.ru>
2023-07-14 13:29:47 +0300

    commit with the result for the task 6.2.1

---
+++ gateway-service/src/main/java/com/example/gatewayservice/filter/RequestHeaderGatewayFilterFactory.java
@@ -0,0 +1,41 @@
+package com.example.gatewayservice.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class RequestHeaderGatewayFilterFactory extends
+        AbstractGatewayFilterFactory<RequestHeaderGatewayFilterFactory.Config> {
+
+    public RequestHeaderGatewayFilterFactory() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            HttpHeaders headers = exchange.getRequest().getHeaders();
+            if (!headers.containsKey(config.getHeaderName())) {
+                exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
+                return exchange.getResponse().setComplete();
+            }
+            return chain.filter(exchange);
+        };
+    }
+
+    public static class Config {
+        private String headerName;
+
+        public String getHeaderName() {
+            return headerName;
+        }
+
+        public void setHeaderName(String headerName) {
+            this.headerName = headerName;
+        }
+    }
+}
